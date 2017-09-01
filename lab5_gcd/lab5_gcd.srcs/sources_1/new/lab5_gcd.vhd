47794af3d21ad25272bac34a3f4c700f4a58d4fb
Arpan-sopho <mangalarpan@gmail.com>
2017-09-01 21:56:38 +0530

    a>b

--- lab5_gcd/lab5_gcd.srcs/sources_1/new/lab5_gcd.vhd
+++ lab5_gcd/lab5_gcd.srcs/sources_1/new/lab5_gcd.vhd
@@ -40,10 +40,13 @@ signal ZERO : std_logic;
 signal checked_valid : std_logic;
 signal load_signal : std_logic;
 signal d : std_logic_vector (15 downto 0);
-shared variable a0_int :integer range 0 to 15;
-shared variable a1_int :integer range 0 to 15;
-shared variable b0_int :integer range 0 to 15;
-shared variable b1_int :integer range 0 to 15;
+signal a0_int :integer range 0 to 15;
+signal a1_int :integer range 0 to 15;
+signal b0_int :integer range 0 to 15;
+signal b1_int :integer range 0 to 15;
+signal d0 : integer range 0 to 15;
+signal d1: integer range 0 to 15;
+
 --signal a : std_logic_vector (7 downto 0);
 --signal b : std_logic_vector (7 downto 0);
 --signal aTOsub : std_logic_vector (7 downto 0);
@@ -53,10 +56,10 @@ begin
 ONE <= '1';
 ZERO <= '0';
 
-a0_int := to_integer(unsigned(a_i(3 downto 0)));
-a1_int := to_integer(unsigned(a_i(7 downto 4)));
-b0_int := to_integer(unsigned(b_i(3 downto 0)));
-b1_int := to_integer(unsigned(b_i(7 downto 4)));
+a0_int <= to_integer(unsigned(a_i(3 downto 0)));
+a1_int <= to_integer(unsigned(a_i(7 downto 4)));
+b0_int <= to_integer(unsigned(b_i(3 downto 0)));
+b1_int <= to_integer(unsigned(b_i(7 downto 4)));
 
 display: lab4_seven_segment_display PORT MAP(
     b => d,
@@ -73,12 +76,20 @@ process(clk,push_i, load)
         if (sub_signal = 1) then
             if(a0_int = b0_int and a1_int = b1_int) then
                 -- the two inputs are equal
-                sub <= '0';
+                sub_signal <= '0';
 
                 sub_signal <= '0';
             elsif(a1_int > b1_int or (a1_int = b1_int and a0_int > b0_int)) then
 					-- A > B
-					if (a0_int > b0_int
-    
+					if (a0_int > b0_int or a0_int = b0_int) then
+							d0 = a0_int - b0_int;
+							d1 = a1_int - b1_int;
+					else
+						d0 = 10 - b0_int + a0_int;
+						d1 = a1_int - b1_int - 1;
+					end if;
+					-- assign the difference to A
+					a0_int <= d0;
+					a1_int <=d1;
 end process;
 end architecture ;
